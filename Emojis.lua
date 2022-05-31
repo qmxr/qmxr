6f41e4f7e7088f0ef6fad077fc1d56049cfe0ad3
qmxr <85464549+qmxr@users.noreply.github.com>
2022-05-31 19:13:10 +0200

    Update Emojis.lua

--- Emojis.lua
+++ Emojis.lua
@@ -1,44 +1,71 @@
-coroutine.resume(coroutine.create(function()
+
+
 local a = "Pewdudi"
-if game.Players:FindFirstChild(a) then    
-    game.Players:FindFirstChild(a).Character.Humanoid.DisplayName = ("[💎]"..game.Players:FindFirstChild(a).Character.Humanoid.DisplayName.. " | Pew")
-end
-local aa = "Gusate666"
-if game.Players:FindFirstChild(aa) then    
-    game.Players:FindFirstChild(aa).Character.Humanoid.DisplayName = ("[💎]"..game.Players:FindFirstChild(aa).Character.Humanoid.DisplayName.. " | Pew's Bro")
-end
-local b = "6tpz"
-if game.Players:FindFirstChild(b) then    
-    game.Players:FindFirstChild(b).Character.Humanoid.DisplayName = ("[😎]"..game.Players:FindFirstChild(b).Character.Humanoid.DisplayName.. " | Kat")
+if game.Players:FindFirstChild(a) then
+    if game.Players:FindFirstChild(a).Character.Humanoid.DisplayName == ("Pew") then
+        game.Players:FindFirstChild(a).Character.Humanoid.DisplayName = ("[💎]"..game.Players:FindFirstChild(a).Character.Humanoid.DisplayName.. " | Pew")
+    end
 end
 
-local c = "i_db"
-if game.Players:FindFirstChild(c) then    
-    game.Players:FindFirstChild(c).Character.Humanoid.DisplayName = ("[🥰]"..game.Players:FindFirstChild(c).Character.Humanoid.DisplayName.. " | Idb :3")
+local b = "Gusate666"
+if game.Players:FindFirstChild(b) then
+    if game.Players:FindFirstChild(b).Character.Humanoid.DisplayName == ("Hallowed") then
+        game.Players:FindFirstChild(b).Character.Humanoid.DisplayName = ("[💎]"..game.Players:FindFirstChild(b).Character.Humanoid.DisplayName.. " | Pew's Bro")
+    end
 end
 
-local d = "Fraudion"
-if game.Players:FindFirstChild(d) then    
-    game.Players:FindFirstChild(d).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(d).Character.Humanoid.DisplayName.. " | Leg")
+local c = "6tpz"
+if game.Players:FindFirstChild(c) then
+    if game.Players:FindFirstChild(c).Character.Humanoid.DisplayName == ("6tpz") then
+        game.Players:FindFirstChild(c).Character.Humanoid.DisplayName = ("[😎]"..game.Players:FindFirstChild(c).Character.Humanoid.DisplayName.. " | Kat")
+    end
 end
 
-local e = "timsece"
-if game.Players:FindFirstChild(e) then    
-    game.Players:FindFirstChild(e).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(e).Character.Humanoid.DisplayName.. " | ages")
+local d = "i_db"
+if game.Players:FindFirstChild(d) then
+    if game.Players:FindFirstChild(d).Character.Humanoid.DisplayName == ("i_db") then
+        game.Players:FindFirstChild(d).Character.Humanoid.DisplayName = ("[🥰]"..game.Players:FindFirstChild(d).Character.Humanoid.DisplayName.. " | :3")
+    end
 end
 
-local ii = "manipulate01"
-if game.Players:FindFirstChild(ii) then    
-    game.Players:FindFirstChild(ii).Character.Humanoid.DisplayName = ("[🥺]"..game.Players:FindFirstChild(ii).Character.Humanoid.DisplayName.. " | Babi")
+local e  = "Fraudion"
+if game.Players:FindFirstChild(e) then
+    if game.Players:FindFirstChild(e).Character.Humanoid.DisplayName == ("Fraudion") then
+        game.Players:FindFirstChild(e).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(e).Character.Humanoid.DisplayName.. " | Leg")
+    end
 end
 
-local gg = "1vaIeriee"
-if game.Players:FindFirstChild(gg) then    
-    game.Players:FindFirstChild(gg).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(gg).Character.Humanoid.DisplayName.. " | HellFire")
+local f  = "timsece"
+if game.Players:FindFirstChild(f) then
+    if game.Players:FindFirstChild(f).Character.Humanoid.DisplayName == (" Iloveliana ") then
+        game.Players:FindFirstChild(f).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(f).Character.Humanoid.DisplayName.. " | ages")
+    end
 end
 
-local g = "rqmane"
-if game.Players:FindFirstChild(g) then    
-    game.Players:FindFirstChild(g).Character.Humanoid.DisplayName = ("[✨]"..game.Players:FindFirstChild(g).Character.Humanoid.DisplayName.. " | Rqmane")
+local g  = "manipulate01"
+if game.Players:FindFirstChild(g) then
+    if game.Players:FindFirstChild(g).Character.Humanoid.DisplayName == ("") then
+        game.Players:FindFirstChild(g).Character.Humanoid.DisplayName = ("[🥺]"..game.Players:FindFirstChild(g).Character.Humanoid.DisplayName.. " | Babi")
+    end
 end
-end))
+
+
+local h = "rqmane"
+if game.Players:FindFirstChild(h) then
+    if game.Players:FindFirstChild(h).Character.Humanoid.DisplayName == ("psychic") then
+        game.Players:FindFirstChild(h).Character.Humanoid.DisplayName = ("[✨]"..game.Players:FindFirstChild(h).Character.Humanoid.DisplayName.. " | rqman")
+    end
+end
+
+local i = "1vaIeriee"
+if game.Players:FindFirstChild(i) then
+    if game.Players:FindFirstChild(i).Character.Humanoid.DisplayName == ("Lixjr") then
+        game.Players:FindFirstChild(i).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(i).Character.Humanoid.DisplayName.. " | HellFire")
+    end
+end
+local j = "veznoq"
+if game.Players:FindFirstChild(j) then
+    if game.Players:FindFirstChild(j).Character.Humanoid.DisplayName == ("hellfire") then
+        game.Players:FindFirstChild(j).Character.Humanoid.DisplayName = ("[😳]"..game.Players:FindFirstChild(j).Character.Humanoid.DisplayName.. " | HellFire")
+    end
+end
