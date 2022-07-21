cb1d0247f3d9a02bd4dd65f671e80bdc782c78e9
qmxr <85464549+qmxr@users.noreply.github.com>
2022-07-21 02:14:32 +0200

    Update Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,48 +1,56 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local tablefind = table.find
-local MainEvent = ReplicatedStorage.MainEvent
-local SpoofTable = {
-    WalkSpeed = 16,
-    JumpPower = 50
-}
-
-local Flags = {
-    "CHECKER_1",
-    "TeleportDetect",
-    "OneMoreTime"
-}
-local __namecall
-__namecall = hookmetamethod(game, "__namecall", function(...)
-    local args = {...}
-    local self = args[1]
-    local method = getnamecallmethod()
-    local caller = getcallingscript()
-
-    if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
-        return
-    end
-
-    if (not checkcaller() and getfenv(2).crash) then
-        hookfunction(getfenv(2).crash, function()
-            warn("Crash Attempt") 
-        end)
-    end
-    return __namecall(...)
+if game.PlaceId == 2788229376 then
+wait(20)
+game.Players.LocalPlayer.PlayerGui.MainScreenGui.Bar.HP.Picture.Life.Visible = true
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+game.Players.LocalPlayer.PlayerGui:WaitForChild("MainScreenGui"):WaitForChild("Bar"):WaitForChild("HP"):WaitForChild("Picture"):WaitForChild("Life").Visible = true
 end)
+local BreathingHAMON = Instance.new("StringValue", game.Players.LocalPlayer.DataFolder.Information)
+BreathingHAMON.Name = "JJARC"
 
-local __index
-__index = hookmetamethod(game, "__index", function(t, k)
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        return SpoofTable[k]
-    end
-    return __index(t, k)
-end)
+function Tool()
+    local Tool = Instance.new('Tool')
+    Tool.Name = 'OverDrive';
+    Tool.RequiresHandle = false;
+    Tool.Parent = game:GetService('Players').LocalPlayer:FindFirstChildWhichIsA('Backpack');
+end;
+game:GetService('Players').LocalPlayer.Character:WaitForChild('FULLY_LOADED_CHAR');
+Tool();
+game:GetService('Players').LocalPlayer.CharacterAdded:Connect(function(v)
+    v:WaitForChild('FULLY_LOADED_CHAR');
+    Tool();
+end);
 
-local __newindex
-__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        SpoofTable[k] = v
-        return
-    end
-    return __newindex(t, k, v)
-end)
+assert(getrawmetatable)
+gmt = getrawmetatable(game)
+setreadonly(gmt, false)
+old = gmt.__namecall
+gmt.__namecall =
+    newcclosure(
+        function(self, ...)
+        local args = {...}
+        if tostring(args[1]) == "BreathingHAMON" then
+            return
+        elseif tostring(args[1]) == "TeleportDetect" then
+            return
+        elseif tostring(args[1]) == "CHECKER_1" then
+            return
+        elseif tostring(args[1]) == "CHECKER" then
+            return
+        elseif tostring(args[1]) == "GUI_CHECK" then
+            return
+        elseif tostring(args[1]) == "OneMoreTime" then
+            return
+        elseif tostring(args[1]) == "checkingSPEED" then
+            return
+        elseif tostring(args[1]) == "BANREMOTE" then
+            return
+        elseif tostring(args[1]) == "PERMAIDBAN" then
+            return
+        elseif tostring(args[1]) == "KICKREMOTE" then
+            return
+        elseif tostring(args[1]) == "BR_KICKPC" then
+            return end
+
+        return old(self, ...)
+    end)
+end
